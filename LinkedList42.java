b80a54d68fbcb353b38a2a1112f89627f8562f57
Anni0p <b.c114303@gmail.com>
2024-07-02 19:11:18 +0530

    Data Structure

---
+++ LinkedList42.java
@@ -0,0 +1,26 @@
+class node
+{
+    public int data;
+    public  node next;
+}
+/*
+struct node 
+{
+    int data;
+    struct node *next;
+};
+*/
+
+class SinglyLL
+{
+    public node First;
+    public int iCount;
+}
+
+class LinkedList42 
+{
+    public static void main(String Google[])
+    {
+        SinglyLL obj = new SinglyLL();
+    }    
+}
